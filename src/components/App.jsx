b7eef460fcd26fd88635e43b87f9f69c4c911cb1
Holubchuk <sashagolubchuk3@gmail.com>
2023-12-23 15:45:22 -0500

    feature/basic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import css from './App.module.css'
+
+
+
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +16,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      Hello
     </div>
   );
 };
